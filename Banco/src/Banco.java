b01e30f5850dbe0fca854242c0075b892107310d
Jules-Dev1 <1jules.sc@gmail.com>
2026-03-05 13:23:07 -0300

    arquivos aula 2

--- Banco/src/Banco.java
+++ Banco/src/Banco.java
@@ -19,13 +19,26 @@ public class Banco {
         c1.saldo = 100;
 
         Conta c2 = new Conta();
-        c2.numero = "5432-1";
+        c2.numero = "2345-6";
         c2.titular = p2;
         c2.limite = 200;
         c2.saldo = 150;
 
-        System.out.println(c1.saldo);
-        System.out.println(c1.titular.nome);
-        System.out.println("Saldo da conta " + c2.numero + ": " + c2.saldo);
+        c1.extrato();
+        c2.extrato();
+
+        c1.sacar(150);
+        c1.transferir(100, c2);
+        c1.sacar(100);
+        c1.depositar(100);
+        c1.transferir(200, c2);
+
+        c1.extrato();
+        c2.extrato();
+
+        for (int i = 1; i <= 120; i++) {
+            c1.chequeEspecial(0.5);
+            System.out.println("Saldo apos " + i + " dia: " + c1.saldo);
+        }
     }
 }
